b121c394b6971517c1f0e4b17edd5f06be6d0afc
realsoumyadey07 <soumyadipdey802@gmail.com>
2025-01-01 13:44:38 +0530

    js object is being done

--- 01_objects/objectMethods.js
+++ 01_objects/objectMethods.js
@@ -3,12 +3,25 @@ const myObj = {};
 const myObj2 = {
      name: "Soumya dey",
      age: 22,
-     nationality: "Indian"
+     nationality: "Indian",
+     get getNationality() {
+          return this.nationality;
+     }
 }
+Object.defineProperty(myObj2, "setNationality", {
+     set: function (value) {
+          this.nationality = value;
+     }
+})
 // assing properties of the object to a new object
 Object.assign(myObj, myObj2);
 // creates an object from an existing object
 const myObj3 = Object.create(myObj2);
 myObj3.name = "Ishan Dey";
-
-console.log(myObj);
+// assessing object key-values
+for(let[key, val] of Object.entries(myObj2)){
+     console.log(`Key is: ${key}, Value is: ${val}`);
+}
+myObj2.setNationality = "Italian";
+const nationality = myObj2.getNationality;
+console.log(nationality);
